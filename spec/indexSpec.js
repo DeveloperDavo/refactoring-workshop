4b2243263386c8096a4df48e748b9c7124c37027
Developer Davo <DeveloperDavo@users.noreply.github.com>
2017-11-16 15:44:17 +0100

    extract determineClassTaxRate

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -11,34 +11,4 @@ describe("index", () => {
     expect(index.calculateTax(61000, 'couple')).toBeCloseTo(0.37 * 61000 * 0.95);
     expect(index.calculateTax(1000000, 'family')).toBeCloseTo(0.55 * 1000000 * 0.90);
   });
-
-  it("should determine income tax rate", () => {
-    const taxClass = 'single';
-    expect(index.determineIncomeTaxRate(19999, taxClass)).toBe(0);
-    expect(index.determineIncomeTaxRate(20000, taxClass)).toBe(0.2);
-    expect(index.determineIncomeTaxRate(60000, taxClass)).toBe(0.37);
-    expect(index.determineIncomeTaxRate(100000, taxClass)).toBe(0.55);
-    expect(index.determineIncomeTaxRate(100001, taxClass)).toBe(0.55);
-  });
-
-  it("should determine income tax rate for couples", () => {
-    const taxClass = 'couple';
-    const taxRate = 0.95;
-    expect(index.determineIncomeTaxRate(19999, taxClass)).toBeCloseTo(0);
-    expect(index.determineIncomeTaxRate(20000, taxClass)).toBeCloseTo(0.2 * taxRate);
-    expect(index.determineIncomeTaxRate(60000, taxClass)).toBeCloseTo(0.37 * taxRate);
-    expect(index.determineIncomeTaxRate(100000, taxClass)).toBeCloseTo(0.55 * taxRate);
-    expect(index.determineIncomeTaxRate(100001, taxClass)).toBeCloseTo(0.55 * taxRate);
-  });
-
-  it("should determine income tax rate for families", () => {
-    const taxClass = 'family';
-    const taxRate = 0.90;
-    expect(index.determineIncomeTaxRate(19999, taxClass)).toBeCloseTo(0);
-    expect(index.determineIncomeTaxRate(20000, taxClass)).toBeCloseTo(0.2 * taxRate);
-    expect(index.determineIncomeTaxRate(60000, taxClass)).toBeCloseTo(0.37 * taxRate);
-    expect(index.determineIncomeTaxRate(100000, taxClass)).toBeCloseTo(0.55 * taxRate);
-    expect(index.determineIncomeTaxRate(100001, taxClass)).toBeCloseTo(0.55 * taxRate);
-  });
-
 });
--- src/index.js
+++ src/index.js
@@ -1,32 +1,41 @@
 class Index {
   calculateTax(income, taxClass) {
-    return income * this.determineIncomeTaxRate(income, taxClass);
-
+    let classTaxRate = this.determineClassTaxRate(taxClass);
+    let incomeTaxRate = this.determineIncomeTaxRate(income);
+    return income * classTaxRate * incomeTaxRate;
   }
 
-  determineIncomeTaxRate(income, taxClass) {
-    let taxRate = 1;
-    if (taxClass === 'couple') {
-      taxRate = 0.95;
-    } else if (taxClass === 'family') {
-      taxRate = 0.9;
-    }
+  determineIncomeTaxRate(income) {
 
     if (income < 20000) {
       return 0;
     }
 
     if (income < 60000) {
-      return 0.2 * taxRate;
+      return 0.2;
     }
 
     if (income < 100000) {
-      return 0.37 * taxRate;
+      return 0.37;
     }
 
     if (income >= 100000) {
-      return 0.55 * taxRate;
+      return 0.55;
     }
 
+  }
+
+  determineClassTaxRate(taxClass) {
+    if (taxClass === 'single') {
+      return 1;
+    }
+
+    if (taxClass === 'couple') {
+      return 0.95;
+    }
+
+    if (taxClass === 'family') {
+      return 0.9;
+    }
   }
 }
